bac8d4643a2858c688142fabef43acfe54d941ec
Ralph Albert Sommer <ralphsommer@gmail.com>
2019-07-21 18:01:51 -0300

    first commit

---
+++ src/main/java/restapi/CityRestApiApplication.java
@@ -0,0 +1,13 @@
+package restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityRestApiApplication.class, args);
+	}
+
+}
